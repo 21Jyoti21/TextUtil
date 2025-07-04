b72ccd1feabbf79127b41d00ce340418f31740e5
Jyoti <210jg021@gmail.com>
2025-07-04 18:38:30 +0530

    Deploying to gh-pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 import Navbar from './components/Navbar';
-import About from './components/About';
+// import About from './components/About';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   const [mode,setMode]=useState('light');//Wether dark mode is enabled or not
@@ -46,19 +46,20 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
       <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-      
       <Alert alert={alert}/>
       <div className='container my-3'>
-
-      <Routes>
-           <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter text" mode={mode} />} />
-        </Routes>
-      {/* <About /> */}
+      <TextForm showAlert={showAlert} heading="Enter text" mode={mode} />
       </div>
-      </Router>
+      {/* <Routes> */}
+           {/* <Route exact path="/about" element={<About />} /> */}
+          {/* <Route exact path="/" element={ */}
+            {/* <TextForm showAlert={showAlert} heading="Enter text" mode={mode} /> */}
+            {/* // } /> */}
+        {/* // </Routes> */}
+      {/* <About /> */}
+      {/* </Router> */}
     </>
   );
 }
